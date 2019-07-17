c6cba8a71eb611ef26e4693ced52825f5635fecd
Lennin666 <soporte@allnexus.com.co>
2019-07-17 11:59:25 -0500

    tejido inga con audios y sugerencias del cliente, faltan detalles

---
+++ MundoInga/Tejido-inga/js/main.js
@@ -0,0 +1,137 @@
+var abuela=document.querySelector('#abuela');
+var rueda=document.querySelector('#ruedaCompleta');
+var fondo=document.querySelector('#moverFondo');
+var simbolo1=document.querySelector('#simbolo-1');
+var simbolo2=document.querySelector('#simbolo-2');
+var simbolo3=document.querySelector('#simbolo-3');
+var letrero1=document.querySelector('#letrero-1');
+var letrero2=document.querySelector('#letrero-2');
+var letrero3=document.querySelector('#letrero-3');
+
+const btnStart = document.getElementById('ruedaCompleta');
+
+
+var audiosTejidos = document.getElementsByClassName('audio');
+// console.log(audiosTejidos);
+// audiosTejidos[0].play();
+
+// simbolo1.classList.add("prueba")
+
+btnStart.addEventListener('click', start);
+abuela.setAttribute("xlink:href", "./img/inga-frente.png");
+
+function start(){
+		btnStart.removeEventListener('click', start);
+		btnStart.classList.remove("simbolo");
+		firstEstation();
+	}
+
+function firstEstation(){
+	fondo.style.transform="translateX(-82px)";
+	abuela.setAttribute("xlink:href", "./img/inga-abuela.png");
+	rueda.classList.add("rodar15")
+	abuela.style.animationName="caminar";
+	setTimeout(()=>{
+		removerFirst();
+	},3500)
+}
+
+function removerFirst(){
+abuela.setAttribute("xlink:href", "./img/inga-frente.png");
+abuela.style.animationName="";
+audiosTejidos[0].play();
+letrero1.classList.add("aparecer");
+simbolo1.classList.add("prueba");
+		setTimeout(()=>{
+		 hideMsnFirst();
+		},15000)
+}
+
+function hideMsnFirst(){
+	letrero1.classList.remove("aparecer");
+		setTimeout(()=>{
+			secondEstation();
+		},1200)
+}
+
+function secondEstation(){
+	fondo.style.transform="translateX(-164px)";
+ 	abuela.setAttribute("xlink:href", "./img/inga-abuela.png");
+ 	rueda.style.transform="rotate(-62deg)";
+ 	abuela.style.animationName="caminar";
+		setTimeout(()=>{
+			removerSecond();
+		},3000)
+}
+
+function removerSecond(){
+	abuela.setAttribute("xlink:href", "./img/inga-frente.png");
+	abuela.style.animationName="";
+	audiosTejidos[1].play();
+	letrero2.classList.add("aparecer");
+	simbolo2.classList.add("prueba");
+		setTimeout(()=>{
+		 hideMsnSecond();
+		},15000)
+}
+
+function hideMsnSecond(){
+	letrero2.classList.remove("aparecer");
+	setTimeout(()=>{
+		
+		thirdEstation();
+	},1200)
+}
+
+function thirdEstation(){
+  fondo.style.transform="translateX(-240px)";
+  abuela.setAttribute("xlink:href", "./img/inga-abuela.png"); 
+  rueda.style.transform="rotate(-80deg)"
+  abuela.style.animationName="caminar";
+		setTimeout(()=>{
+			removerThird();
+  	},3000)
+}
+
+function removerThird(){
+	
+	abuela.setAttribute("xlink:href", "./img/inga-frente.png");
+	abuela.style.animationName="";
+	audiosTejidos[2].play();
+	letrero3.classList.add("aparecer");
+	simbolo3.classList.add("prueba");
+		setTimeout(()=>{
+			hideMsnSThird();
+		},15000)
+}
+
+function hideMsnSThird(){
+	letrero3.classList.remove("aparecer");
+		setTimeout(()=>{
+			exitEstation();
+		},1200)
+}
+
+function exitEstation(){
+	fondo.style.transform="translateX(-330px)";
+	abuela.setAttribute("xlink:href", "./img/inga-abuela.png"); 
+  rueda.style.transform="rotate(-138deg)";
+  abuela.style.animationName="caminar";
+  	setTimeout(()=>{		
+		final();	
+ 		},3500)
+}
+
+function final(){
+	abuela.setAttribute("xlink:href", "./img/inga-frente.png");
+	abuela.style.animationName="";
+	audiosTejidos[3].play();
+}
+
+
+
+
+
+
+
+
