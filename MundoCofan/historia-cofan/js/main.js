aed0214e4c3ec8dc6cb65171d02cb74a0038b3fa
Lennin666 <soporte@allnexus.com.co>
2019-07-18 11:13:02 -0500

    historia cofan

--- MundoCofan/historia-cofan/js/main.js
+++ MundoCofan/historia-cofan/js/main.js
@@ -1,24 +1,24 @@
 const fondo = document.getElementById('mover');
-const btnCanoa = document.getElementById('canoa');
+const btnArbol = document.getElementById('arbol');
 const btnRueda = document.getElementById('ruedaBn');
 const abuela = document.getElementById('abuela');
 
-const audios = document.getElementsByClassName('audio');
+const audios = document.getElementsByClassName('audios');
 
-abuela.setAttribute("xlink:href", "./img/siona-frente.png");
+abuela.setAttribute("xlink:href", "./img/kofan-frente.png");
 
 btnRueda.addEventListener('click', inicio);
 
 function inicio(){
 	fondo.classList.add('mover');
 	btnRueda.classList.add('rueda');
-	abuela.setAttribute("xlink:href", "./img/siona-abuela.png");
+	abuela.setAttribute("xlink:href", "./img/kofan-abuela.png");
 	abuela.classList.add('abuela');
 	btnRueda.removeEventListener('click', inicio);
 	btnRueda.classList.remove('click');
 
 	var movimiento=setTimeout(function(){
-		abuela.setAttribute("xlink:href", "./img/siona-frente.png");
+		abuela.setAttribute("xlink:href", "./img/kofan-frente.png");
 		//
 		abuela.classList.add("saludo");
 		
@@ -30,24 +30,22 @@ function inicio(){
 }
 
 function personajeSaludo(){
-	abuela.setAttribute("xlink:href", "./img/siona-saludo.png");
+	abuela.setAttribute("xlink:href", "./img/kofan-saludo.png");
 	audios[0].play();
 	setTimeout(()=>{
 	 personajeStop();
 	},5000)
  }
 
- function personajeStop(){
-	abuela.setAttribute("xlink:href", "./img/siona-frente.png");
-	btnCanoa.classList.add("prueba");
-	btnCanoa.style.opacity = "1";
-	btnCanoa.classList.add('click');
- }
+function personajeStop(){
+	abuela.setAttribute("xlink:href", "./img/kofan-frente.png");
+	btnArbol.classList.add("prueba");
+	btnArbol.style.opacity = "1";
+	btnArbol.classList.add('click');
+}
 
-
-btnCanoa.addEventListener("click", function(){
-btnCanoa.style.display="none"
+btnArbol.addEventListener("click", function(){
+btnArbol.style.display="none"
 audios[1].play();
 })
 	
-
