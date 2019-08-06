8fab3034cddc0488c5b63293fbebfcb179312fe3
lenninIbarraGonzalez <soporte@allnexus.com.co>
2019-08-06 16:17:35 -0500

    multi explorador creditos y participantes

--- Inicio/creditos/js/main.js
+++ Inicio/creditos/js/main.js
@@ -2,12 +2,14 @@
 const volver=document.getElementById('volver');
 const credito=document.getElementById('texto');
 
+credito.setAttribute("xlink:href", "./img/creditos/creditos.png");
+credito.setAttribute("width","740");
+
+
 volver.addEventListener("click", function(){
 window.open("../../TejiendoSaberes.html", "_self");
 })
 
-credito.setAttribute("xlink:href", "./img/creditos/creditos.png",);
-credito.setAttribute("width","740");
 
 
 
--- Inicio/participantes/js/main.js
+++ Inicio/participantes/js/main.js
@@ -1,5 +1,11 @@
 
+const credito=document.getElementById('texto');
 var volver=document.getElementById('volver');
+
+credito.setAttribute("xlink:href", "./img/participantes/participantes.png");
+credito.setAttribute("width","740");
+
+
 volver.addEventListener("click", function(){
   window.open("../../TejiendoSaberes.html", "_self");
 })
