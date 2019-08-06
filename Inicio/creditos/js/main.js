2a9a25f8a7b0839681c6cf942fa4a72bf1d4f906
Lennin666 <soporte@allnexus.com.co>
2019-08-06 11:49:12 -0500

    cambio tejiendo saberes multiexplorer falta arregalr creditos multiexplorer

--- Inicio/creditos/js/main.js
+++ Inicio/creditos/js/main.js
@@ -1,9 +1,13 @@
 
-var volver=document.getElementById('volver');
+const volver=document.getElementById('volver');
+const credito=document.getElementById('texto');
+
 volver.addEventListener("click", function(){
 window.open("../../TejiendoSaberes.html", "_self");
 })
 
+credito.setAttribute("xlink:href", "./img/creditos/creditos.png",);
+credito.setAttribute("width","740");
 
 
 
